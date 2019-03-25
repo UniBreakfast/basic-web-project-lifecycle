16393aa5a26990d88437815fddf9424dedbc2363
unibreakfast <unibreakfast1@gmail.com>
2019-03-25 16:29:03 +0200

    \r\n or \r\n - doesn't matter

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetch('index.txt')
   .then(file => file.text())
   .then(text => text.split(text.includes('\r\n\r\n')? '\r\n\r\n' : '\n\n')
-    .map(block => block.split('\r\n  '))
+    .map(block => block.split(block.includes('\r\n  ')? '\r\n  ' : '\n  '))
     .filter(block => block.length>1)
     .map(block => ({header: block.shift(), moves: block
       .reduce((str, move) => str+`<li>${move}</li>\n`, '\n')}))
