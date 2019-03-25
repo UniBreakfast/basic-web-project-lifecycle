51dc474de9405041bef77afcbbe2c8e5462ae9ba
unibreakfast <unibreakfast1@gmail.com>
2019-03-25 16:26:46 +0200

    \r\n or

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 fetch('index.txt')
   .then(file => file.text())
-  .then(text => text.split('\r\n\r\n')
+  .then(text => text.split(text.includes('\r\n\r\n')? '\r\n\r\n' : '\n\n')
     .map(block => block.split('\r\n  '))
     .filter(block => block.length>1)
     .map(block => ({header: block.shift(), moves: block
       .reduce((str, move) => str+`<li>${move}</li>\n`, '\n')}))
     .map(step =>
       `\n<li>${step.header}\n<ul class=moves>${step.moves}</ul>\n</li>`))
+  .then(steps => {console.log(steps); return(steps)})
   .then(steps =>
     document.querySelector('.steps').innerHTML = steps.join('')+'\n')
