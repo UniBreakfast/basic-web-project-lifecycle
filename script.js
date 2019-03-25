4a1d5d7e5e29c2141131cd6d7ab1903c95ca3004
unibreakfast <unibreakfast1@gmail.com>
2019-03-25 16:16:20 +0200

    wording, styles and formatting tweaks

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 fetch('index.txt')
-  .then(file => file.text().split('\r\n\r\n'))
-  .then(blocks => blocks
+  .then(file => file.text())
+  .then(text => text.split('\r\n\r\n')
     .map(block => block.split('\r\n  '))
     .filter(block => block.length>1)
     .map(block => ({header: block.shift(), moves: block
-      .reduce((str, move) => str+`<li>${move}</li>`, '')}))
-    .map(step => `<li>${step.header}<ul class=moves>${step.moves}</ul></li>`))
-  .then(steps => document.querySelector('.steps').innerHTML = steps.join(''))
+      .reduce((str, move) => str+`<li>${move}</li>\n`, '\n')}))
+    .map(step =>
+      `\n<li>${step.header}\n<ul class=moves>${step.moves}</ul>\n</li>`))
+  .then(steps =>
+    document.querySelector('.steps').innerHTML = steps.join('')+'\n')
