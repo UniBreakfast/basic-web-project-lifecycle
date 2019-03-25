493b5fbeb45397f7580bf3d45d3273943709e33a
unibreakfast <unibreakfast1@gmail.com>
2019-03-25 10:18:07 +0200

    index.txt parser refactoring

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 fetch('index.txt')
-  .then(file => file.text())
-  .then(text => text.split('\r\n\r\n'))
-  .then(blocks => blocks.map(block => block.split('\r\n  '))
+  .then(file => file.text().split('\r\n\r\n'))
+  .then(blocks => blocks
+    .map(block => block.split('\r\n  '))
     .filter(block => block.length>1)
-    .map(lines => {
-      const step = lines.reduce((step, line, move) => { move
-          ? (step.moves? step.moves.push(line) : step.moves = [line])
-          : step.header = line
-        return step
-      }, {})
-      step.moves = step.moves.map(move => '<li>'+move+'</li>')
-      return `<li>${step.header}
-        <ul class=moves>${step.moves.join('')}</ul>
-      </li>`
-    } )
-    .join('')
-  )
-  .then(steps => document.querySelector('.steps').innerHTML = steps)
+    .map(block => ({header: block.shift(), moves: block
+      .reduce((str, move) => str+`<li>${move}</li>`, '')}))
+    .map(step => `<li>${step.header}<ul class=moves>${step.moves}</ul></li>`))
+  .then(steps => document.querySelector('.steps').innerHTML = steps.join(''))
